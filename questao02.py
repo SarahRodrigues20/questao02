9000bf2bbb222e273d8bdd24779651023a886a4f
SarahRodrigues20 <113391554+SarahRodrigues20@users.noreply.github.com>
2022-09-30 07:56:54 -0300

    Add files via upload

---
+++ questao02.py
@@ -0,0 +1,16 @@
+n1  =  float ( input ( 'Digite a 1ª média: ' ))
+n2  =  float ( input ( 'Digite a 2ª média: ' ))
+n3  =  float ( input ( 'Digite a 3ª média: ' ))
+
+media  = ( n1  +  n2  +  n3 ) /  3
+exame  =  6  -  media
+
+if   media  < 3 :
+    print ( "REPROVADO" )
+elif  media  >= 3 e media < 6 :
+    print ( f"Fará um EXAME, presisa de { exame } para ser APROVADO" )
+elif > = 6:
+    print ( "APROVADO" )
+
+elif > = 7:
+    print ( "APROVADO" )
